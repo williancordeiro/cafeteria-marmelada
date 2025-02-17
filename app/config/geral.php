a198b4a49e279c745f387d27c190442a392329e6
williancordeiro <williancordeiro369@gmail.com>
2025-02-16 22:59:04 -0300

    Upload

---
+++ app/config/geral.php
@@ -0,0 +1,21 @@
+<?php
+    const SITE_NAME = 'Will Coffe';
+
+    const URL_RAIZ = '/willcoffee.com.br/app';
+
+    define('RAIZ', dirname(__DIR__) . '/');
+
+    const CONFIG_DIR = RAIZ . 'config/';
+
+    const MVC_DIR = RAIZ . 'src/';
+
+    const CONTROLLER_DIR = MVC_DIR . 'Controller/';
+
+    const MODEL_DIR = MVC_DIR . 'Model/';
+
+    const VIEW_DIR = MVC_DIR . 'View/';
+
+    const PUBLIC_DIR = RAIZ . 'public/';
+
+    const URL_PUBLIC = RAIZ . 'public/';
+    const URL_CSS = URL_PUBLIC . 'css/';
---
+++ app/src/View/templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= URL_CSS . 'style.lcss' ?>">
+    <title>Will Coffe</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <a href="<?= URL_RAIZ ?>">ICONE</a>
+            </div>
+            <div>
+                <h2>WILL COFFE Cafeteria</h2>
+            </div>
+            <div>
+                <ol>
+                    <li>Menu</li>
+                    <li>Login</li>
+                    <li>Contato</li>
+                </ol>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
