429d9fd8beae0627982d932ba03a29f6ec52ab04
Willian Cordeiro <williancordeiro369@gmail.com>
2025-04-23 22:54:29 +0000

    ItemModel

---
+++ app/src/Model/ItemModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace Model;
+
+require_once 'config/data-base.php';
+require_once DATABASE_DIR . 'DataBase.php';
+
+// var_dump($dataBase);
+
+use \PDO;
+use \PDOException;
+use DataBase\DataBase;
+
+class ItemModel {
+    
+}
