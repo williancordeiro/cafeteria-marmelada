3032ef08b53ebb925940c7edfb889516e49359ec
Willian Cordeiro <williancordeiro369@gmail.com>
2025-04-27 18:07:25 +0000

    home-css

--- app/src/View/Home/home.php
+++ app/src/View/Home/home.php
@@ -8,6 +8,8 @@ require_once 'config/geral.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= URL_CSS . 'reset.css' ?>">
+    <link rel="stylesheet" href="<?= URL_CSS . 'style.css' ?>">
     <title><?= SITE_NAME ?></title>
 </head>
 <body>
