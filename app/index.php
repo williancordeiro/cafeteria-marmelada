d0344a4d472eec229948f703c30c7a684c62418e
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-03 21:53:38 +0000

    item-form-edit update 3

--- app/index.php
+++ app/index.php
@@ -4,7 +4,8 @@ require_once 'config/geral.php';
 require_once 'config/routers.php';
 require_once CONTROLLER_DIR . 'Controller.php';
 
-$request_uri = $_SERVER['REQUEST_URI'];
+//$request_uri = $_SERVER['REQUEST_URI'];
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $request_method = $_SERVER['REQUEST_METHOD'];
 
 if ($request_uri === '/' && isset($_SESSION['user_id'])) {
