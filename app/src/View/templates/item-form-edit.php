6586b54186db473e2f56790e28b6624429cb55de
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-03 22:09:43 +0000

    item-form-edit update 4

--- app/src/View/templates/item-form-edit.php
+++ app/src/View/templates/item-form-edit.php
@@ -1,11 +1,11 @@
 <form action="<?= URL_RAIZ . 'products/edit?id=' . $item->getId() ?>" method="post" class="form">
     <div class="form-group">
         <label for="name">Nome:</label>
-        <input type="text" name="name" id="name" valeu="<?= htmlspecialchars($item->getName()) ?>" autofocus required>
+        <input type="text" name="name" id="name" value="<?= htmlspecialchars($item->getName()) ?>" autofocus required>
     </div>
     <div class="form-group">
         <label for="price">Preço:</label>
-        <input type="number" name="price" id="price" step="0.01" min="0" valeu="<?= $item->getPrice() ?>" required>
+        <input type="number" name="price" id="price" step="0.01" min="0" value="<?= $item->getPrice() ?>" required>
     </div>
     <div class="form-group">
         <label for="qtd">Quantidade:</label>
