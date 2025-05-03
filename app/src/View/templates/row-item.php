e1e7e8ab0b876d82f0464c044b3740fec8df3d4b
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-03 22:42:55 +0000

    delete item update 5

--- app/src/View/templates/row-item.php
+++ app/src/View/templates/row-item.php
@@ -4,8 +4,8 @@
     <td>R$ <?= number_format($preco, 2, ',', '.') ?></td>
     <td><?= $qtd ?></td>
     <td>
-        <a href="<?= URL_RAIZ . 'products/delete?id=' . $id ?>" class="edit-btn">✏️</a>
-        <form action="<?= URL_RAIZ . 'products/delete?id=' . $id ?>" method="post" onsubmit="return confirm('Tem certeza que deseja excluir este produto?')">
+        <a href="<?= URL_RAIZ . 'products/edit?id=' . $id ?>" class="edit-btn">✏️</a>
+        <form action="<?= URL_RAIZ . 'products/delete?id=' . $item->getId() ?>" method="post" onsubmit="return confirm('Tem certeza que deseja excluir este produto?')">
             <button class="delete-btn" type="submit">🗑️</button>
         </form>
     </td>
