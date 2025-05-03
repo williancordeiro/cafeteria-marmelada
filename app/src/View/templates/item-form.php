3bd66b888570fd8589d3012032a9f86f525f5823
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-03 01:12:38 +0000

    item-form update

--- app/src/View/templates/item-form.php
+++ app/src/View/templates/item-form.php
@@ -1,3 +1,14 @@
-<form action="<?= URL_RAIZ . '/products/register' ?>">
-
+<form action="<?= URL_RAIZ . '/products/register' ?>" method="post" class="form">
+    <div class="form-group">
+        <label for="name">Nome:</label>
+        <input type="text" name="name" id="name" autofocus required>
+    </div>
+    <div class="form-group">
+        <label for="price">Preço:</label>
+        <input type="number" name="price" id="price" step="0.01" min="0" required>
+    </div>
+    <div class="form-group">
+        <label for="qtd">Quantidade:</label>
+        <input type="number" name="qtd" id="qtd" min="0" max="100" required>
+    </div>
 </form>
