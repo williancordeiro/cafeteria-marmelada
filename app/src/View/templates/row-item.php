d54039743f60c93c2cff66816ee8d44c96b90451
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-03 02:30:39 +0000

    item-edit error

--- app/src/View/templates/row-item.php
+++ app/src/View/templates/row-item.php
@@ -4,7 +4,7 @@
     <td>R$ <?= number_format($preco, 2, ',', '.') ?></td>
     <td><?= $qtd ?></td>
     <td>
-        <a href="<?= URL_RAIZ . 'products/edit=?id=' . $id ?>" class="edit-btn">✏️</a>
+        <a href="<?= URL_RAIZ . 'products/edit?id=' . $id ?>" class="edit-btn">✏️</a>
         <button class="delete-btn">🗑️</button>
     </td>
 </tr>
