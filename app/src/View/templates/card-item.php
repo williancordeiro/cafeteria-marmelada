b24f3609a3c1e223806796480848fd428a8e9d5b
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-04 16:28:52 +0000

    final updates 11

--- app/src/View/templates/card-item.php
+++ app/src/View/templates/card-item.php
@@ -5,8 +5,8 @@
         <p class="item-name"><?= $nome ?></p>
 
         <div class="item-details">
-            <p class="item-price">Preço: <span>R$ <?= number_format($preco, 2, ',', '.') ?></span> </p>
-            <p class="item-quantity">Quantidade: <span><?= $qtd ?></span></p>
+            <p class="item-price">Preço<span>R$ <?= number_format($preco, 2, ',', '.') ?></span> </p>
+            <p class="item-quantity">Quantidade<span><?= $qtd ?></span></p>
         </div>
     </div>
 </div>
