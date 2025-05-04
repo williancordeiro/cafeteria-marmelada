36017142d43c9a5629be336d47624d5bf551b7a2
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-04 21:10:12 +0000

    final updates 14

--- app/src/View/Product/product.php
+++ app/src/View/Product/product.php
@@ -16,28 +16,30 @@
             <div class="add-btn-wrapper">
                 <a href="<?= URL_RAIZ . 'products/register' ?>" class="add-btn">Adicionar +</a>
             </div>
-            <table class="item-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Preço</th>
-                        <th>Quantidade</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($items as $item): ?>
-                        <?php
-                            $id = $item['id'];
-                            $nome = $item['nome'];
-                            $preco = $item['preco'];
-                            $qtd = $item['qtd'];
-                            include VIEW_DIR . 'templates/row-item.php';
-                        ?>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+            <div class="table-scroll">
+                <table class="item-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Preço</th>
+                            <th>Quantidade</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($items as $item): ?>
+                            <?php
+                                $id = $item['id'];
+                                $nome = $item['nome'];
+                                $preco = $item['preco'];
+                                $qtd = $item['qtd'];
+                                include VIEW_DIR . 'templates/row-item.php';
+                            ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
 
